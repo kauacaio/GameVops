758ccd891a73953b9d3c84755f8a72696507f8cc
kaua.lima <kaua.lima@greenn.com.br>
2024-11-12 21:31:28 -0300

    Commit espetacular

--- mario.js
+++ mario.js
@@ -19,34 +19,27 @@ const jump = () => {
 };
 
 const startGame = () => {
-    // Redefine a página atual, reiniciando o jogo
     location.reload();
 };
 
-// Reinicia o jogo ao clicar no botão
 restartButton.addEventListener('click', startGame);
 
-// Loop do jogo
-// Função para criar fogos de artifício
 function createFireworks() {
     for (let i = 0; i < 10; i++) {
         const firework = document.createElement('div');
         firework.classList.add('firework');
         
-        // Definindo posição aleatória para o "fogo de artifício"
         firework.style.left = `${Math.random() * 100}%`;
         firework.style.animationDelay = `${Math.random() * 2}s`;  // Variedade no tempo de início
         
         fireworksContainer.appendChild(firework);
         
-        // Remover fogos de artifício depois de terminar a animação
         setTimeout(() => {
             firework.remove();
         }, 1500);
     }
 }
 
-// Loop do jogo
 const gameLoop = setInterval(() => {
     if (!isGameOver) {
         const pipePosition = pipe.offsetLeft;
@@ -58,42 +51,37 @@ const gameLoop = setInterval(() => {
         if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
             isGameOver = true;
 
-            // Pausa a animação do pipe e do Mario
             pipe.style.animation = 'none';
             pipe.style.left = `${pipePosition}px`;
             mario.style.animation = 'none';
             mario.style.bottom = `${marioPosition}px`;
 
-            // Troca a imagem do Mario para "Game Over"
             mario.src = 'game-over.png';
             mario.style.width = '75px';
             mario.style.marginLeft = '50px';
             gameOverText.style.display = 'block';
             restartButton.style.display = 'block';
 
-            // Atualiza a maior pontuação
             const highScore = localStorage.getItem('highScore') || 0;
             if (score > highScore) {
                 localStorage.setItem('highScore', score);
 
-                // Mostra a animação do novo recorde
                 newRecordText.style.display = 'block';
 
-                // Cria os fogos de artifício
+              
                 createFireworks();
 
-                // Esconde a mensagem após 3 segundos
+             
                 setTimeout(() => {
                     newRecordText.style.display = 'none';
-                }, 3000); // A animação do recorde dura 3 segundos
+                }, 3000); 
             }
         }
     }
 }, 10);
 
 
-// Evento para pular ao pressionar qualquer tecla
 document.addEventListener('keydown', jump);
 
-// Inicia o jogo ao clicar em "Jogar Novamente"
+
 restartButton.addEventListener('click', startGame);
